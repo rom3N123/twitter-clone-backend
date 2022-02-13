bc37f77bc04e947d32bce1c1c19918b447837333
Martynov <r.martynov@gk-adept.ru>
2022-02-13 11:02:51 +0300

    feat: make socket connection

--- src/index.js
+++ src/index.js
@@ -5,22 +5,39 @@ import cors from 'cors';
 import './core/db.js';
 import './configs/cloudinaryConfig.js';
 import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const CORS_ORIGIN = 'http://localhost:3000';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: CORS_ORIGIN,
+	},
+});
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
 app.use(
-    cors({
-        credentials: true,
-        origin: 'http://localhost:3000',
-    }),
+	cors({
+		credentials: true,
+		origin: CORS_ORIGIN,
+	})
 );
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
+io.on('connection', socket => {
+	console.log('a user connected');
+	console.log(socket);
+});
+
 app.use('/api', ApiRouter);
 
-app.listen(PORT, () => {
-    console.log('server has started');
+server.listen(PORT, () => {
+	console.log('server has started');
 });
